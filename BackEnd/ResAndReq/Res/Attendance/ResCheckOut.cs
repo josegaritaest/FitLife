4a4a01ec82ed3ecf5ffda0bed6d410cfb132bc21
MónicaSC <monica.sccr@gmail.com>
2025-05-18 21:23:03 -0600

    Attendance
    Apis:
    -api/attendance/status
    -api/attendance/checkout
    -api/attendance/checkin

---
+++ BackEnd/ResAndReq/Res/Attendance/ResCheckOut.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.ResAndReq.Res.Attendance
+{
+    public class ResCheckOut : ResBase
+    {
+        public string CheckInTime { get; set; }
+        public string CheckOutTime { get; set; }
+        public int? DurationMinutes { get; set; }
+    }
+}
